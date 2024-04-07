32667374a85dea7137a7cac879404ca46e8f165a
lena <lenarichards2234@gmail.com>
2024-04-07 03:30:39 +0100

    made getimage to work, it's 330am i want to sleep

--- src/commands/utility/image.js
+++ src/commands/utility/image.js
@@ -1,3 +1,19 @@
+require('dotenv').config({ path: '../../../.env' })
+const { SlashCommandBuilder } = require('discord.js');
 const pg = require('pg')
 const { Client } = pg
 const client = new Client()
+
+async function login() {
+	await client.connect()
+}
+login()
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('getimage')
+		.setDescription('queries db for an image'),
+	async execute(interaction) {
+		await interaction.reply('positive');
+	},
+};
